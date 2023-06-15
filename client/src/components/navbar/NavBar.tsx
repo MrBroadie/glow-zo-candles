a10f3583ddb9cd0dbe13172263cc822b9fc026f5
Tom Broad <thomas.broad@ecs.co.uk>
2023-06-15 16:27:22 +0100

    navbar styling

--- client/src/components/navbar/NavBar.tsx
+++ client/src/components/navbar/NavBar.tsx
@@ -10,6 +10,11 @@ import LogoutButton from "./LogoutButton";
 
 const NavBar = () => {
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   const [userState, setUserState] = useState<userObject>({
     sub: "",
@@ -27,25 +32,83 @@ const NavBar = () => {
   console.log("nav", userState);
 
   const styleWords =
-    "block p-4 text-lg hover:bg-rose-50 hover:text-rose-300 rounded border-2 border-white hover:border-rose-300";
+    "block p-2 text-lg  hover:bg-rose-50 hover:text-rose-300 rounded border-white w-full";
   const styleIcons = "w-10 hover:cursor-pointer";
 
   return (
-    <nav className="sticky top-0 px-4 py-1 sm:flex flex-wrap bg-white drop-shadow-sm w-full z-10">
-      <div className="sm:flex flex-row justify-around items-center w-full hover:cursor-pointer">
-        <button
-          onClick={() => navigate("/")}
-          className="sm:inline-flex px-4 py-4 font-semibold hover:bg-rose-50 hover:text-rose-300 rounded border-2 border-white hover:border-rose-300"
-        >
-          <img src={candle} alt="logo" className={styleIcons} />
-          <span className="self-end">GlowZo</span>
-        </button>
-        <ul className="sm:inline-flex sm:space-x-16">
+    <>
+      <nav className="sticky top-0 px-4 py-1 bg-white h-20 drop-shadow-sm z-10">
+        <div className="flex justify-between items-center w-full ">
+          <button
+            onClick={() => navigate("/")}
+            className="flex px-4 py-4 rounded font-semibold"
+          >
+            <img src={candle} alt="logo" className={styleIcons} />
+            <span className="self-end">GlowZo</span>
+          </button>
+          {/* Show when large screen */}
+          <ul className="hidden lg:flex w-1/2">
+            <button
+              onClick={() => navigate("/collection")}
+              className={styleWords}
+            >
+              Collection
+            </button>
+            <button onClick={() => navigate("/")} className={styleWords}>
+              Home
+            </button>
+            <button onClick={() => navigate("/about")} className={styleWords}>
+              About
+            </button>
+            <button onClick={() => navigate("/contact")} className={styleWords}>
+              Contact us
+            </button>
+          </ul>
+          <div className="hidden lg:flex space-x-4 p-4">
+            <BasketButton styleIcons={styleIcons} />
+            {userState.sub.length ? (
+              <LogoutButton styleIcons={styleIcons} />
+            ) : (
+              <LoginButton styleIcons={styleIcons} />
+            )}
+          </div>
+
+          {/* Show when small screen */}
+          <button onClick={toggleMenu} className="lg:hidden">
+            <div className={`flex flex-col h-full justify-center items-end`}>
+              <span
+                className={`h-0.5 w-8 my-2 bg-slate-500 transform transition duration-500 ease-in-out ${
+                  isOpen ? "rotate-45 translate-y-2.5" : ""
+                }`}
+              ></span>
+              <span
+                className={`h-0.5 w-8 bg-slate-500 transform transition duration-500 ease-in-out ${
+                  isOpen ? "opacity-0" : ""
+                }`}
+              ></span>
+              <span
+                className={`h-0.5 w-8 bg-slate-500 my-2 transform transition duration-500 ease-in-out ${
+                  isOpen ? "-rotate-45 -translate-y-2.5" : ""
+                }`}
+              ></span>
+            </div>
+          </button>
+        </div>
+      </nav>
+      <div
+        className={`transition duration-200 ease-in-out transform p-2 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        } fixed top-20 right-0 z-20 w-64  overflow-auto bg-white h-fit rounded p-4 lg:hidden`}
+      >
+        <ul>
           <button
             onClick={() => navigate("/collection")}
             className={styleWords}
           >
             Collection
+          </button>
+          <button onClick={() => navigate("/")} className={styleWords}>
+            Home
           </button>
           <button onClick={() => navigate("/about")} className={styleWords}>
             About
@@ -54,16 +117,16 @@ const NavBar = () => {
             Contact us
           </button>
         </ul>
-        <div className="w-full inline-flex space-x-4 sm:w-auto">
+        <div className="w-full flex justify-around space-x-4 p-4">
+          <BasketButton styleIcons={styleIcons} />
           {userState.sub.length ? (
             <LogoutButton styleIcons={styleIcons} />
           ) : (
             <LoginButton styleIcons={styleIcons} />
           )}
-          <BasketButton styleIcons={styleIcons} />
         </div>
       </div>
-    </nav>
+    </>
   );
 };
 
